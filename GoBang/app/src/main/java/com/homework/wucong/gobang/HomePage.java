8b563959871f1dd6aeb2a3b980165b61c051ba0f
wucong9827 <wucong9827@github.com>
2019-11-28 11:53:14 +0800

    first init

---
+++ GoBang/app/src/main/java/com/homework/wucong/gobang/HomePage.java
@@ -0,0 +1,27 @@
+package com.homework.wucong.gobang;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.homework.wucong.gobang.activities.PlayActivity;
+
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+    /**
+     * 跳转到游戏界面
+     * @param view
+     */
+    public void playOne(View view){
+        Intent intentPlay = new Intent(HomePage.this,PlayActivity.class);
+        startActivity(intentPlay);
+    }
+}
---
+++ GoBang/app/src/main/java/com/homework/wucong/gobang/activities/PlayActivity.java
@@ -0,0 +1,15 @@
+package com.homework.wucong.gobang.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.homework.wucong.gobang.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
